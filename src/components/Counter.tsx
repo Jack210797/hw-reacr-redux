8493f0711f77e171cdb33f8f177d49844a5d8daa
Jack210797 <suvorov21079@gmail.com>
2024-07-03 15:53:45 +0300

    Redux2

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,12 +1,13 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { decrement, increment } from '../redux/actions'
 import { CounterState } from '../redux/types'
+import '../index.css'
 
 const Counter = () => {
   const count = useSelector((state: CounterState) => state.count)
   const dispatch = useDispatch()
   return (
-    <div>
+    <div className="container">
       <h1>Count: {count}</h1>
       <button onClick={() => dispatch(increment())}>Increment</button>
       <button onClick={() => dispatch(decrement())}>Decrement</button>
